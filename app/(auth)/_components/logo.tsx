5ba0dbeee913f50ca877f024f462963f714389c1
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-10 08:38:58 +0530

    added dark mode to auth
    changed the layout of clerk auth using clerk themes , also made username compulsory  if the user is login in to the app for the first time

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["200" , "300", "400",  "500",  "600",  "700",  "800"]
+});
+
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+        <div className="bg-white rounded-full p-1">
+            <Image 
+                src= "/spooky-removebg.svg"
+                alt="Streamhub"
+                height="80"
+                width="80"
+            />
+        </div>
+        <div className={cn(
+            "flex flex-col items-center",
+            font.className
+        )}>
+            <p className="text-xl font-semibold">
+                Streamhub
+            </p>
+            <p className="text-sm text-muted-foreground">
+                Let&apos;s Stream
+            </p>
+        </div>
+    </div>
+  );
+};
